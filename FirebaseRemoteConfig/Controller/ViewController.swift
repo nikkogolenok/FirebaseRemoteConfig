c5751cb7371ecbfc2509d0cab375f05127de7f24
nikita_kogolenok <nikiton17ns@gmail.com>
2022-03-29 09:41:40 +0300

    Initial Commit

---
+++ FirebaseRemoteConfig/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FirebaseRemoteConfig
+//
+//  Created by Никита Коголенок on 29.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
